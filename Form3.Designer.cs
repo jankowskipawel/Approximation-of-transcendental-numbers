8c3914ffad9e750a0d529bc980b43888c21053f9
warminski <warminpiotr@gmail.com>
2020-10-21 16:51:30 +0200

    finished colliding blocks

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -32,10 +32,16 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -45,40 +51,23 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 12);
+            this.button1.Location = new System.Drawing.Point(729, 107);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(739, 71);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(724, 150);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(29, 25);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(742, 146);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(745, 174);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label1.Text = "Pi";
             // 
             // pictureBox1
             // 
@@ -89,20 +78,93 @@
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(729, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(729, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Wybierz długość Pi";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.numericUpDown1.Location = new System.Drawing.Point(749, 29);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(36, 32);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(35, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "masa malego bloku";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(410, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "masa dużego bloku";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(72, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(443, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(819, 296);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(841, 296);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form3";
             this.Text = "Form1";
-            
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form3_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,9 +175,14 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form3.cs
+++ Form3.cs
@@ -16,19 +16,21 @@ namespace ApproximationOfTranscendentalNumbers
     {
         Graphics g;
         Bitmap btm;
-        int counter = 0;
-        float timesteps = 100;
+        
+        double counter = 0;
+        double timesteps;
         Block small;
         Block large;
+        int digits;
         
        
         public struct Block
         {
-            public float x;
-            public float width;
-            public float mass;
-            public float velocity;
-            public Block(float x,float width, float mass, float velocity)
+            public double x;
+            public double width;
+            public double mass;
+            public double velocity;
+            public Block(double x, double width, double mass, double velocity)
             {
                 this.x = x;
                 this.width = width;
@@ -50,10 +52,10 @@ namespace ApproximationOfTranscendentalNumbers
                     return true;
                 }
             }
-            public float ElasticCollision(Block other)
+            public double ElasticCollision(Block other)
             {
-                float sum = mass + other.mass;
-                float newVelocity = (mass - other.mass) / sum * velocity + (2 * other.mass / sum) * other.velocity;
+                double sum = mass + other.mass;
+                double newVelocity = (mass - other.mass) / sum * velocity + (2 * other.mass / sum) * other.velocity;
                 return newVelocity;
             }
             public bool Wall()
@@ -72,14 +74,28 @@ namespace ApproximationOfTranscendentalNumbers
 
         public Form3()
         {
-            small = new Block(100, 20, 1, 0);
-            large = new Block(300, 80, 100000000, -1/timesteps);
             InitializeComponent();
+            digits = Convert.ToInt32(numericUpDown1.Value);
+            timesteps = Math.Pow(10, digits);
+            if(timesteps > 100000)
+            {
+                timesteps = 500000;
+            }
+            if (digits > 8)
+            {
+                timesteps = 5000000;
+            }
+            small = new Block(100, 20, 1, 0);
+            large = new Block(300, 80, Math.Pow(100,digits-1), -2/timesteps);
         }
         public void DrawBlocks(Block small, Block large)
         {
-            RectangleF block1 = new RectangleF(large.x, 119, large.width, large.width);
-            RectangleF block2 = new RectangleF(small.x, 179, small.width, small.width);
+            if(large.x < small.x+small.width)
+            {
+                large.x = small.x + small.width;
+            }
+            RectangleF block1 = new RectangleF((float)large.x, 119, (float)large.width, (float)large.width);
+            RectangleF block2 = new RectangleF((float)small.x, 179, (float)small.width, (float)small.width);
             btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(btm);
             SolidBrush b = new SolidBrush(Color.Red);
@@ -89,50 +105,74 @@ namespace ApproximationOfTranscendentalNumbers
             
         }
 
-        
-        
-        
-        
-
-
-
         private void button1_Click(object sender, EventArgs e)
         {
+
+            digits = Convert.ToInt32(numericUpDown1.Value);
+
+            timesteps = Math.Pow(10,digits);
+            if (digits > 5)
+            {
+                timesteps = 500000;
+            }
+            if(digits > 8)
+            {
+                MessageBox.Show("to chwile potrwa");
+                timesteps = 5000000;
+            }
+            counter = 0;
+            small = new Block(100, 20, 1, 0);
+            large = new Block(300, 80, Math.Pow(100, digits - 1), -2 / timesteps);
+            label5.Text = small.mass.ToString();
+            label6.Text = large.mass.ToString();
+            DrawBlocks(small, large);
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Text = counter.ToString();
             
             for(int i = 0; i < timesteps; i++)
             {
                 if(small.IsColliding(large))
                 {
-                    float v1 = small.ElasticCollision(large);
-                    float v2 = large.ElasticCollision(small);
+                    double v1 = small.ElasticCollision(large);
+                    double v2 = large.ElasticCollision(small);
                     small.velocity = v1;
                     large.velocity = v2;
                     counter++;
+                    
+                    
                 }
                 if(small.Wall())
                 {
                     small.velocity *= -1;
                     counter++;
+                    
                 }
             
                 small.UpdatePos();
                 large.UpdatePos();
 
+
             }
+            
+            label1.Text = counter.ToString();
             DrawBlocks(small, large);
-
-
-
-
 
         }
 
-       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            
+        }
+
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+        }
     }
+
 }
+
