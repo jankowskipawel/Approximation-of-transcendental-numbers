d5ea7a4aa15ad9f8140e0f0fcea7ded12d387821
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2020-11-19 15:43:17 +0100

    Drawing pi

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -33,58 +33,205 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(65, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(624, 426);
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 496);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // timer1
             // 
-            this.timer1.Interval = 10;
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(702, 65);
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(1094, 295);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(18, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "0";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(684, 28);
+            this.button1.Location = new System.Drawing.Point(1071, 6);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1080, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(1094, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PI: ";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, -2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1237, 599);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1152, 573);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1229, 573);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1054, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(36, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1000, 500);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(1065, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(1065, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "e";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1039, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "0";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1042, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1235, 629);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form4";
             this.Text = "Form4";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form4_FormClosing);
             this.Load += new System.EventHandler(this.Form4_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -94,5 +241,17 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -14,19 +15,30 @@ namespace ApproximationOfTranscendentalNumbers
     public partial class Form4 : Form
     {
         Graphics g;
+        Graphics g2;
         Bitmap btm;
+        Bitmap btm2;
         Pen p;
-        double pi = 4;
+        Pen p2;
+        Brush b1 = new SolidBrush(Color.DarkMagenta);
+        float pi = 4;
         int i = 0;
-        List<double> memory = new List<double>();
-        PointF[] points;
+        List<float> memory = new List<float>();
+        List<float> memory2 = new List<float>();
+        double ojler = 0;
+        ulong sumCounter = 0;
+        ulong x = 0;
+
         public Form4()
         {
             
             InitializeComponent();
             btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(btm);
-            p = new Pen(Brushes.White);
+            btm2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            g2 = Graphics.FromImage(btm2);
+            p = new Pen(Brushes.White,2);
+            p2 = new Pen(Brushes.Red);
         }
 
 
@@ -34,44 +46,44 @@ namespace ApproximationOfTranscendentalNumbers
         {
             
             timer1.Start();
+            memory.Add(3.5f);
+           
         }
-        /*public void Draw(List<double>memory)
+        public void Draw(List<float> memory)
         {
-            double spacing = pictureBox1.Width / memory.Count();
-            for (int i = 0; i < memory.Count();i++)
+            for (int i = 1; i < memory.Count();i++)
             {
-                double x = i * spacing;
-                double y = memory[i];
-                
-                g.DrawLine(p,)
-                
+                float spacing = pictureBox1.Width / (float)memory.Count();
+                float x = i * spacing;
+                g.DrawLine(p, x-spacing, memory[i - 1]*400-1030, x, memory[i]*400-1030);
+                g.DrawLine(p2, 0, (float)Math.PI*400-1030, pictureBox1.Width, (float)Math.PI * 400-1030);
                 pictureBox1.Image = btm;
-                
-                
             }
-        }*/
-        public double Remap(double value, double from1, double to1, double from2, double to2)
-        {
-            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            double divider = i * 2 + 3;
+            
+            float denominator = i * 2 + 3;
             if(i % 2 == 0)
             {
-                pi -= (4 / divider);
+                pi -= (4 / denominator);
             }
             else
             {
-                pi += (4 / divider);
+                pi += (4 / denominator);
+            }
+            if (memory.Count >= pictureBox1.Width/8)
+            {
+                memory.RemoveAt(0);
             }
             memory.Add(pi);
             label1.Text = memory.Count().ToString();
-            
-            
-            
             i++;
+            g.Clear(Color.Black);
+            label2.Text = memory[memory.Count-1].ToString();
+            Draw(memory);
+            
         }
 
         private void Form4_FormClosing(object sender, FormClosingEventArgs e)
@@ -83,5 +95,62 @@ namespace ApproximationOfTranscendentalNumbers
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Draw(memory);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            x = Convert.ToUInt64(textBox1.Text);
+            timer2.Start();
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            float spacing = pictureBox2.Width / (float)memory.Count();
+            if (sumCounter < 20)
+            {
+                ojler += Math.Pow(x, sumCounter) / (double)Factorial(sumCounter);
+                sumCounter++;
+            }
+            memory2.Add((float)ojler);
+            label6.Text = ojler.ToString();
+            label4.Text = sumCounter.ToString();
+            g.Clear(Color.Black);
+            DrawOjler(memory2);
+        }
+
+        public void DrawOjler(List<float> memoryOjler)
+        {
+            for (int i = 1; i < memoryOjler.Count(); i++)
+            {
+                //float spacing = pictureBox2.Width / (float)memoryOjler.Count();
+                //float xd = i * spacing;
+                g2.DrawLine(p, i - 1, memoryOjler[i - 1], i, memoryOjler[i]);
+                g2.DrawLine(p2, 0, (float)Math.E * 1500 - 3925, pictureBox2.Width, (float)Math.E * 1500 - 3925);
+                pictureBox2.Image = btm2;
+            }
+        }
+
+
+        public ulong Factorial(ulong x)
+        {
+            ulong result = 1;
+            for (ulong i = x; i > 0; i--)
+                result *= i;
+            return result;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Factorial(20).ToString());
+        }
+        
     }
 }
